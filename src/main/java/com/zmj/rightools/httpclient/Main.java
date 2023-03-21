21866181201f1360e6981c16e61dcc3118895a41
zmj <1312970787@qq.com>
2023-03-21 22:00:56 +0800

    结构

---
+++ src/main/java/com/zmj/rightools/httpclient/Main.java
@@ -0,0 +1,11 @@
+package com.zmj.rightools.httpclient;
+
+/**
+ * @author zhangmingjian
+ * @date 2023/3/19
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
